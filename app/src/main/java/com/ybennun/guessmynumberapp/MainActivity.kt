afd4a4d9672d8c711a9024f712498f5516413021
YeshayahuBennun <ybennun@yahoo.com>
2021-09-11 21:23:22 +0300

    commmit#5 - Create Functions and Use 'String.format' to             Print Strings

--- app/src/main/java/com/ybennun/guessmynumberapp/MainActivity.kt
+++ app/src/main/java/com/ybennun/guessmynumberapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.ybennun.guessmynumberapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var number: Int = 5
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        question_text_view.text = String.format(resources.getString(R.string.str_question), number)
+
+
+    }
+
+    fun clickDownwardArrow(view: View) {
+        //TODO
+    }
+
+    fun clickSuccess(view: View) {
+        //TODO
+    }
+
+    fun clickUpwardArrow(view: View) {
+        //TODO
+    }
+
+    fun clickResetButton(view: View) {
+        //TODO
     }
 }
