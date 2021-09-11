4cf26ece92405e37dad3dbc325c27cdf26aa21e5
YeshayahuBennun <ybennun@yahoo.com>
2021-09-11 23:36:54 +0300

    commmit#6 - Implement the Complete Algorithm Logic

--- app/src/main/java/com/ybennun/guessmynumberapp/MainActivity.kt
+++ app/src/main/java/com/ybennun/guessmynumberapp/MainActivity.kt
@@ -13,24 +13,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        question_text_view.text = String.format(resources.getString(R.string.str_question), number)
-
-
+        printTheQuestion()
     }
 
     fun clickDownwardArrow(view: View) {
-        //TODO
+        if (number == 5) {
+            guessTheNumber(3)
+        } else if (number == 3) {
+            guessTheNumber(2)
+        } else if (number == 2) {
+            guessTheNumber(1)
+        } else if (number == 8) {
+            guessTheNumber(7)
+        } else if (number == 7) {
+            guessTheNumber(6)
+        }
     }
 
     fun clickSuccess(view: View) {
-        //TODO
+        question_text_view.visibility = View.INVISIBLE
+        result_text_view.visibility = View.VISIBLE
+        result_text_view.text = String.format(resources.getString(R.string.str_success), number)
     }
 
     fun clickUpwardArrow(view: View) {
-        //TODO
+        if (number == 3) {
+            guessTheNumber(4)
+        } else if (number == 5) {
+            guessTheNumber(8)
+        } else if (number == 8) {
+            guessTheNumber(9)
+        } else if (number == 9) {
+            guessTheNumber(10)
+        }
     }
 
     fun clickResetButton(view: View) {
-        //TODO
+        result_text_view.visibility = View.INVISIBLE
+        question_text_view.visibility = View.VISIBLE
+        number = 5
+        printTheQuestion()
+    }
+
+    private fun printTheQuestion() {
+        question_text_view.text = String.format(resources.getString(R.string.str_question), number)
+    }
+
+    private fun guessTheNumber(myNumber: Int) {
+        number = myNumber
+        printTheQuestion()
     }
 }
